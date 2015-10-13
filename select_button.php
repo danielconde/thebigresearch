c8c5f85f35b2370c8430e7c19e1bb22cc15fb5fe
root <root@dani.hostalia.com>
2015-10-13 16:06:41 +0200

    lat

--- select_button.php
+++ select_button.php
@@ -1,8 +1,6 @@
-
 <html>
 <head>
 <title>A BASIC HTML FORM</title>
-
 
 <?php
 $servername = "localhost";
@@ -18,27 +16,31 @@ if ($conn->connect_error) {
 } 
 
 
-$sql = "SELECT r1, r2 FROM t_questions WHERE id='1'";
+$cont=3;
+$cont=intval($cont);
+
+$sql = "SELECT question, r1, r2 FROM t_questions WHERE id=$cont";
 $result = $conn->query($sql);
+
 
 if ($result->num_rows > 0) {
 
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        
-#Almaceno las variables r1 y r2 para mostrar en los botones
-$submit_post1=$row["r1"];
-$submit_post2=$row["r2"];
+//Output of data row
+while($row = $result->fetch_assoc()) {
 
-#	echo  "Preguntas: " . $row["r1"]. " " . $row["r2"]. "<br>";
-    }
-} else {
-    echo "0 results";
+ $submit_post1=$row["r1"];
+ $submit_post2=$row["r2"];
+
+ }
+} 
+else {
+echo "0 results";
 }
 #$conn->close();
 
-
 ?>
+
+
 
 </head>
 </head>
@@ -52,24 +54,22 @@ $submit_post2=$row["r2"];
 </body>
 </html>
 
+
+
 <?php
 
-
     if (isset($_POST['submit1'])) {
-    //Incrementamos la respuest
-//    $sql_insert = "UPDATE `tbr_db`.`t_questions` SET `r1c` = r1c+1 WHERE `t_questions`.`id` = 1"; //No hace falta
-//    $result = $conn->query($sql_insert);
-    //Insertamos la respuesta del usuario
-      $sql_insert_usuario = "INSERT INTO t_user(username, id_u, id_p, r1, r2, date) VALUES('usertest','u1','1','1','0', NOW())";
-      $result = $conn->query($sql_insert_usuario); 
+      $sql_insert_user = "INSERT INTO t_user(username, id_u, id_p, r1, r2, date) VALUES('usertest','u1','1','1','0', NOW())";
+      $result = $conn->query($sql_insert_user); 
     }
     elseif (isset($_POST['submit2'])) {
-      $sql_insert_usuario = "INSERT INTO t_user(username, id_u, id_p, r1, r2, date) VALUES('usertest','u1','1','0','1', NOW())";
-      $result = $conn->query($sql_insert_usuario);
+      $sql_insert_user = "INSERT INTO t_user(username, id_u, id_p, r1, r2, date) VALUES('usertest','u1','1','0','1', NOW())";
+      $result = $conn->query($sql_insert_user);
     }
 
-      $sql_select_num_preguntas = "SELECT id_p FROM t_user WHERE r1 = '1' ";
-      $result = $conn->query($sql_select_num_preguntas);
+      $sql_select_num_questions = "SELECT id_p FROM t_user WHERE r1 = '1' ";
+      $result = $conn->query($sql_select_num_questions);
       $rowcount=mysqli_num_rows($result);
-      printf("Resultado: %d rows",$rowcount);
+      printf("Result: %d rows",$rowcount);
+      $cont+=1
 ?>
